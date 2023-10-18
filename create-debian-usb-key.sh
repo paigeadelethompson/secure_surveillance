cb6954003a3ba52f5a76d5fec8e09456c5f63995
Paige Thompson <paige@paige.bio>
2023-10-18 16:11:46 -0700

    Update create-debian-usb-key.sh

--- create-debian-usb-key.sh
+++ create-debian-usb-key.sh
@@ -11,7 +11,7 @@ parted ${DISK} mkpart primary fat32 1 256
 parted ${DISK} name 1 BOOT
 parted ${DISK} mkpart primary ext2 256 3000
 parted ${DISK} name 2 ROOT
-parted ${DISK} mkpart primary fat32 3000 4700
+parted ${DISK} mkpart primary fat32 3000 -1
 parted ${DISK} name 3 HOME
 parted ${DISK} set 1 boot on
 
